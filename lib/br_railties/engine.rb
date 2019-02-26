84082ac5d7e5e35f0e15c385d5ee4e5452fb359d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-02-26 00:50:26 -0300

    Fix migration appending.

--- lib/br_railties/engine.rb
+++ lib/br_railties/engine.rb
@@ -5,10 +5,8 @@ module BrRailties
     isolate_namespace BrRailties
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
+      config.paths['db/migrate'].expanded.each do |expanded_path|
+        app.config.paths['db/migrate'] << expanded_path
       end
     end
   end
