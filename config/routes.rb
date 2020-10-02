7706c6e12f012d6c5b7ca2beef896ef46d2318f5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-01 22:41:38 -0300

    config/routes.rb: replace "as_routes" by "concerns: :active_scaffold'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 BrRailties::Engine.routes.draw do
-  resources(:federal_units) { as_routes }
-  resources(:municipalities) do
-    as_routes
-    record_select_routes
-  end
+  concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
+  resources(:federal_units, concerns: :active_scaffold)
+  resources(:municipalities, concerns: :active_scaffold) { record_select_routes }
 end
