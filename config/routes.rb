41bbe3e1ae39b3f58c457b2e52da873236a139d6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 01:47:03 -0300

    config/routes.rb: adapt to "eac_active_scaffold" '0.1.0'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 BrRailties::Engine.routes.draw do
-  concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
-  resources(:federal_units, concerns: :active_scaffold)
-  resources(:municipalities, concerns: :active_scaffold) { record_select_routes }
+  resources(:federal_units, concerns: active_scaffold)
+  resources(:municipalities, concerns: active_scaffold) { record_select_routes }
 end
