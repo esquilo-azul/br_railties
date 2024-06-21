798c4f7b06ecf3540c0368b226a05f35f43ec5b1
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2024-06-21 11:04:31 -0300

    BrRailties::FederalUnit: remove.

--- db/migrate/20190226015936_add_ibge_code_to_br_railties_federal_units.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddIbgeCodeToBrRailtiesFederalUnits < (
-    Rails.version < '5' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
-  )
-  def change
-    add_column :br_railties_federal_units, :ibge_code, :integer
-  end
-end
