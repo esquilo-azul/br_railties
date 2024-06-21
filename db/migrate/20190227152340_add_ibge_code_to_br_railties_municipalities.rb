56deb25582f9dca5958d780219a74836724889b0
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2024-06-21 11:03:08 -0300

    BrRailties::Municipality: remove.

--- db/migrate/20190227152340_add_ibge_code_to_br_railties_municipalities.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddIbgeCodeToBrRailtiesMunicipalities < (
-    Rails.version < '5' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
-  )
-  def change
-    add_column :br_railties_municipalities, :ibge_code, :integer
-  end
-end
