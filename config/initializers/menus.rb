b7cff9eba3cd63a3fe6d32255964bcd3a492c4a5
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2024-06-21 11:01:09 -0300

    BrRailties::MunicipalitiesController:

--- config/initializers/menus.rb
+++ config/initializers/menus.rb
@@ -2,5 +2,4 @@
 
 ::Rails.application.root_menu.group(:admin).group(:br_railties, :br_railties).within do |g|
   g.action(:federal_units).label(-> { ::BrRailties::FederalUnit.plural_name })
-  g.action(:municipalities).label(-> { ::BrRailties::Municipality.plural_name })
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@
 
 BrRailties::Engine.routes.draw do
   resources(:federal_units, concerns: active_scaffold)
-  resources(:municipalities, concerns: active_scaffold) { record_select_routes }
 end
